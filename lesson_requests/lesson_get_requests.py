6d156eba98d4d5eb79cfac1871149de3e6166f25
m.korol <m.korol@geomix.ru>
2025-05-30 15:12:14 +0700

    Решал задачи

---
+++ lesson_requests/lesson_get_requests.py
@@ -0,0 +1,85 @@
+from pydoc import text
+
+import requests
+
+# response = requests.get('https://api.github.com')
+# if response.status_code == 200:
+#     print("Запрос успешно выполнен")
+# else:
+#     print(f"Ошибка: {response.status_code}")
+
+"""
+Выполни GET-запрос к публичному API (например, https://api.agify.io/?name=michael). 
+Проверь, что ответ успешен и что в ответе есть поле age.
+"""
+
+# response = requests.get('https://api.agify.io/?name=michael')
+#
+# if response.status_code == 200:
+#     print(f"Код запроса: {response.status_code}")
+#     data = response.json()  # Сохраняем JSON-ответ в переменную
+#     if 'age' in data:  # Проверка, что в data есть поле 'age'
+#         print(f"Возврат: {data['age']}")
+#     else:
+#         print("Поле 'age' не найдено в ответе")
+# else:
+#     print(f"Ошибка: {response.status_code}")
+
+
+"""
+Отправь GET-запрос с несколькими query-параметрами (например, country_id, name)
+и проверь, что они корректно отражаются в теле ответа.
+"""
+
+# params = {'country': 'USA', 'name': 'michael'}
+# response = requests.get('https://api.agify.io/', params=params)
+#
+# if response.status_code == 200:
+#     print(f"Код запроса: {response.status_code}")
+#     data = response.json()
+#
+#     # Проверяем, что имя из параметров есть в ответе
+#     if data.get('name') == params['name']:
+#         print(f"Параметр name в ответе совпадает: {data.get('name')}")
+#     else:
+#         print(f"Параметр name в ответе НЕ совпадает: {data.get('name')}")
+#     # Проверяем, что  API agify.io не поддерживает параметр country и не возвращает его в ответе
+#     if data.get('country') == params['country']:
+#         print(f"Параметр country в ответе совпадает: {data.get('country')}")
+#     else:
+#         print(f"Параметр country в ответе НЕ совпадает: {data.get('country')}")
+#
+# else:
+#     print(f"Ошибка: {response.status_code}")
+
+"""
+Отправь GET-запрос с кастомными HTTP-заголовками. 
+Убедись, что сервер получил эти заголовки (например, через публичный https://httpbin.org/headers).
+"""
+
+# headers = {'Authorization': 'JWT Token 123', 'Custom-Header': 'value', 'Name-Site': 'LOL'}
+# response = requests.get('https://httpbin.org/headers', headers=headers)
+#
+# if response.status_code == 200:
+#     print(f"Код запроса: {response.status_code}")
+#     data = response.json()
+#
+#     # Проверяем, что кастомные заголовки есть в ответе
+#     if data['headers'].get('Authorization') == headers['Authorization']:
+#         print(f"Параметр Authorization совпадает: {data['headers'].get('Authorization')}")
+#     else:
+#         print(f"Не совпадает")
+#
+#     if data['headers'].get('Custom-Header') == headers['Custom-Header']:
+#         print(f"Параметр Custom-Header совпадает: {data['headers'].get('Custom-Header')}")
+#     else:
+#         print(f"Не совпадает")
+#
+#     if data['headers'].get('Name-Site') == headers['Name-Site']:
+#         print(f"Параметр Name-Site совпадает: {data['headers'].get('Name-Site')}")
+#     else:
+#         print(f"Не совпадает")
+
+"""
+
+"""
